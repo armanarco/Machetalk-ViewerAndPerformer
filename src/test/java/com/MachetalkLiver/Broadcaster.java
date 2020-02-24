9cf943d8fb77c6985594921804369ae7ef17ee77
Arman <fdc.armanarco@gmail.com>
2020-02-24 19:55:22 +0800

    adding sikuli framewoks for flash objects

--- src/test/java/com/MachetalkLiver/Broadcaster.java
+++ src/test/java/com/MachetalkLiver/Broadcaster.java
@@ -1,13 +1,17 @@
 package com.MachetalkLiver;
-import Flash.FlashObjectWebDriver;
+
 import com.CommonFunctions.AllowPlugin;
 import org.junit.*;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,6 +39,17 @@ public class Broadcaster {
     }
 
 
+//    @Test
+//    public void aw() throws FindFailed {
+//        Screen s = new Screen();
+//
+//        Pattern allowImg = new Pattern("Desktop:\\chrome.png");
+//
+//        s.wait(allowImg, 2000);
+//        s.click();
+//        s.click();
+//    }
+
 
 //    @AfterClass
 //    public void afterTest() {
@@ -42,7 +57,7 @@ public class Broadcaster {
 //    }
 
     @Test
-    public void doLogin() throws InterruptedException {
+    public void doLogin() throws InterruptedException, FindFailed {
 
         //allow flash,microphone, camera
         String url = "https://dev-front.machetalk.jp/liver/";
@@ -76,10 +91,45 @@ public class Broadcaster {
         //microphone
 //        driver.findElement(By.xpath("//*[contains(@label,'Default - Microphone (HD Webcam C270) (046d:0825)')]")).click();
 
+        System.out.println("Location of the confirm button.");
+        System.out.println("Size: "+driver.findElement(By.id("js-configCoverHide")).getSize());
+        System.out.println("Location: "+driver.findElement(By.id("js-configCoverHide")).getLocation());
         driver.findElement(By.id("js-configCoverHide")).click();//confirm broadcast
-        FlashObjectWebDriver flashapp = new FlashObjectWebDriver(driver, "live_caster_ready");
 
-        Integer.parseInt(flashapp.callFlashObject("Play"));
+        System.out.println("Location of the live viewer.");
+        System.out.println("Size: "+driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]")).getSize());
+        System.out.println("Location: "+driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]")).getLocation());
+
+        double mult = (double) driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]")).getSize().width  * 0.485084;
+        double mult2 = (double) driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]")).getSize().height * 0.612765;
+
+        System.out.print("Height: "+ mult);
+        System.out.print("Height:"+ mult2);
+        System.out.print("Height: "+ (int)mult);
+        System.out.print("Height:"+ (int)mult2);
+        Actions clicker = new Actions(driver);
+//        clicker.moveToElement(driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]"))).moveByOffset((int)mult, (int)mult2).click().perform();
+        int test = 0;
+        int test1 = 1;
+//        while (test < 470){
+//            clicker.moveToElement(driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]"))).moveByOffset((int)mult, test).click().perform();
+//            System.out.println(test1++);
+//            test++;
+//        }
+//        driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]")).
+//        WebElement ele = driver.findElement(By.xpath("//*[@id=\"live_caster_ready\"]"));
+//        Actions builder = new Actions(driver);
+//        builder.moveToElement(ele, (int)mult, (int)mult2).click().build().perform();
+
+
+
+        Thread.sleep(3000);
+        Screen s = new Screen();
+
+        Pattern allowImg = new Pattern("allow.PNG");
+
+        s.click(allowImg);
+        s.click(allowImg);
 
     }
 
